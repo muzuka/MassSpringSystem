7db40975e4f8ff4c8f76de81a7262a75acbd32fa
muzuka <seanbrown7935@gmail.com>
2015-11-03 13:44:18 -0700

    Finished comment

--- main.cpp
+++ main.cpp
@@ -12,6 +12,12 @@
  * k - real number (spring constant)
  * l - real number (distance of rest state)
  * 
+ * Force on particle p
+ *   Fp = sum of forces connected to p
+ * 
+ * Force between particles
+ *   Fp-pi = -spring.k(||p - pi|| - spring.l)(p - pi)/(||p - pi||)
+ * 
  */
 
 
