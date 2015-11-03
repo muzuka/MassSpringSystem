d0ce0d304f5c57103efbb60965339e682eebfcb5
muzuka <seanbrown7935@gmail.com>
2015-11-03 13:38:29 -0700

    Added main file

---
+++ main.cpp
@@ -0,0 +1,25 @@
+/*
+ * main.cpp
+ * 
+ * Written by: Sean Brown
+ * 
+ * Particle:
+ * x, y, z - vectors of 3 (position
+ * m - real number (mass)
+ * 
+ * Spring:
+ * index i, j - integer (particles)
+ * k - real number (spring constant)
+ * l - real number (distance of rest state)
+ * 
+ */
+
+
+#include <iostream>
+
+int main(int argc, char **argv)
+{
+	
+	return 0;
+}
+
