8139cdaa17dc78589f3b0a5776caf50c9ba09ac3
muzuka <seanbrown7935@gmail.com>
2015-11-05 01:17:19 -0700

    added new functions (getters, setters, and constructors)

--- Particle.h
+++ Particle.h
@@ -7,9 +7,26 @@
  * Written by: Sean Brown
  * 
  */
+
+ #include <Vector.h>
  
  class Particle {
-   
+ private:
+   Vector pos;
+   double mass;
+
+ public:
+ 	Particle();
+ 	Particle(Vector);
+ 	Particle(Vector, double);
+
+ 	Vector getPosition();
+ 	double getMass();
+
+ 	void setPosition(Vector);
+ 	void setPosition(double, double, double);
+ 	void setMass(double);
+
  };
 
 #endif
--- Spring.h
+++ Spring.h
@@ -1,8 +1,27 @@
 #ifndef _SPRING_H_
 #define _SPRING_H_
 
-class Spring {
+#include <Vector.h>
 
+class Spring {
+private:
+	int i, j;
+	double k;
+	double l;
+
+public:
+	Spring();
+	Spring(int, int, double, double);
+
+	Vector getFirst();
+	Vector getSecond();
+	double getConstant();
+	double getLength();
+
+	void setFirst(int);
+	void setSecond(int);
+	void setConstant(double);
+	void setLength(double);
 };
 
 #endif
