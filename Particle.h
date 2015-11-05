43f3e2e5746aa49c8eba73f11ce2e5522d29fe0d
muzuka <seanbrown7935@gmail.com>
2015-11-05 13:18:42 -0700

    added color to Particle

--- Particle.h
+++ Particle.h
@@ -8,23 +8,29 @@
  * 
  */
 
- #include <Vector.h>
+ #include "Vector.h"
  
  class Particle {
  private:
    Vector pos;
+   Vector color;
    double mass;
 
  public:
- 	Particle();
- 	Particle(Vector);
- 	Particle(Vector, double);
+ 	Particle();                         // defaults: color = white, mass = 1.0f, pos = (0, 0, 0)
+  Particle(Vector);                   // (pos)
+ 	Particle(Vector, Vector);           // (pos, color)
+  Particle(Vector, double);           // (pos, mass)
+ 	Particle(Vector, Vector, double);   // (pos, color, mass)
 
  	Vector getPosition();
+  Vector getColor();
  	double getMass();
 
  	void setPosition(Vector);
  	void setPosition(double, double, double);
+  void setColor(Vector);
+  void setColor(double, double, double);
  	void setMass(double);
 
  };
